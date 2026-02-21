efda239fcd0d119b6c1c2f31bcd89292476889a5
Arismendi17 <143583214+Arismendi17@users.noreply.github.com>
2026-02-21 17:44:15 -0400

    project advancements and structure defined

--- rfcypher.c
+++ rfcypher.c
@@ -1,12 +1,45 @@
 #include <stdio.h>
+#include <string.h>
 #include "rail_fence_cypher.h"
 
+FILE* encrypt(FILE* inputFile);
+FILE* decrypt(FILE* inputFile);
+void showHelp(void);
+char* possibleModes[] = {"-e","-d","-h", "-?", "--help"};
+
 int main(int argc, char* argv[])
-{
-    for (int i = 0; i < argc; i++)
-    {
-        printf("%s \n", argv[i]);
-    }
+{ 
+    char* inputFileName;
+    
+    if (argc < 2)
+        printf("\nUsage: rfcypher [mode] [target file name] -o [output file name] \nOr use rfcypher --help\n\n"); 
+   
+    // TODO: handle possible argv missuses
+    // TODO: open input text file
+    // TODO: handle possible modes
+    // TODO: handle output file writing
+
+    char* testString = "abcdefghijklmnopqrstuvwxyz1234567890";
     
     return 0;
+}
+
+FILE* encrypt(FILE* inputFile)
+{
+    // TODO: encryption function
+    FILE* outputFile;
+    return outputFile;
+}
+
+FILE* decrypt(FILE* inputFile)
+{
+    // TODO: decryption function
+    FILE* outputFile;
+    return outputFile;
+}
+
+void showHelp(void)
+{
+    // TODO: write short doc
+    return;
 }
