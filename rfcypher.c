66a66a86070d3c1e6c71fd90574d28c4d64f250f
Arismendi17 <143583214+Arismendi17@users.noreply.github.com>
2026-02-24 16:30:28 -0400

    mode validation implemented

--- rfcypher.c
+++ rfcypher.c
@@ -5,22 +5,41 @@
 FILE* encrypt(FILE* inputFile);
 FILE* decrypt(FILE* inputFile);
 void showHelp(void);
-char* possibleModes[] = {"-e","-d","-h", "-?", "--help"};
+char* possibleModes[] = {"-e", "-d", "-h", "-?", "--help"};
+int lenModes = (sizeof(possibleModes)/sizeof(possibleModes[0]));
+char* errorMsg = "\nUsage: rfcypher [mode] [target file name] -o [output file name] \nOr use rfcypher --help\n\n";
 
 int main(int argc, char* argv[])
 { 
     char* inputFileName;
     
     if (argc < 2)
-        printf("\nUsage: rfcypher [mode] [target file name] -o [output file name] \nOr use rfcypher --help\n\n"); 
+    {
+        printf("%s", errorMsg); 
+        return 1;
+    }
    
-    // TODO: handle possible argv missuses
+    // TODO: handle possible argv missuses (done)
+    int validOption = 0;
+    for (int i = 0; i < lenModes ; i++)
+    {
+        if (strcmp(argv[1], possibleModes[i]) == 0)
+        {
+            validOption = 1;
+            break;
+        }
+    }
+    if (!validOption)
+    {
+        printf("\nInvalid option '%s'%s", argv[1],errorMsg);  
+        return 1;
+    }
+    
     // TODO: open input text file
     // TODO: handle possible modes
     // TODO: handle output file writing
 
     char* testString = "abcdefghijklmnopqrstuvwxyz1234567890";
-    
     return 0;
 }
 
